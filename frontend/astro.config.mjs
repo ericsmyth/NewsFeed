787531b06b67fefa4531995f1888d737d316654c
Eric Smith <eric@Erics-MacBook-Pro.local>
2025-04-20 10:27:06 -0600

    added tailwind and typescript

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   server: {
@@ -10,5 +12,9 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
